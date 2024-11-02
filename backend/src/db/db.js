3723550ed1694ca7717cd713f0c97d79f1de3870
Anh Thu <thub2103565@student.ctu.edu.vn>
2024-11-02 22:12:13 +0700

    Cb deloy dự án

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
+
 const MySQL = require('mysql2');
 
 const connection = MySQL.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '14102003',
-    database: 'babyhouse'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '14102003',
+    database: process.env.DB_NAME || 'babyhouse'
 });
 
 connection.connect((err) => {
     if (err) {
-        console.log(err);
+        console.log('Error connecting to the database:', err);
     } else {
         console.log('Connected to the database');
     }
